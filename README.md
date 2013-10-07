080e8181594af2dfe025f681b74df1338606ea7c
Yifei Zhou <zhouyifei@me.com>
2013-10-07 16:29:28 +0800

    Add a screenshot and modified the README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# TimeIconView
+
+A custom UIView which can display a time with two arc.
+<img src="https://github.com/AustinChou/TimeIconView/raw/master/Demo.png" alt="TimeIconView Screenshot" width="640" height="960" />
+
+## Requrements
+- Xcode 5 or higher
+- iOS 6.0 or higher
+
+## Demo
+
+Build and run the TimeIcon.xcodeproj
--- TimeIcon/ViewController.m
+++ TimeIcon/ViewController.m
@@ -11,8 +11,6 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet TimeIconView *clockView;
-
 @end
 
 @implementation ViewController
@@ -22,8 +20,18 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    self.clockView.HH = 18;
-    self.clockView.MM = 30;
+    TimeIconView *amClock = [[TimeIconView alloc] initWithFrame:CGRectMake(50, 50, 90, 90)];
+    amClock.HH = 7;
+    amClock.MM = 10;
+    amClock.backgroundColor = [UIColor whiteColor];
+    
+    TimeIconView *pmClock = [[TimeIconView alloc] initWithFrame:CGRectMake(170, 50, 90, 90)];
+    pmClock.HH = 18;
+    pmClock.MM = 40;
+    pmClock.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:amClock];
+    [self.view addSubview:pmClock];
     
 }
 
